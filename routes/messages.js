b50933fc78b5ce9c2fd2dd1f9b49de204ed0e20a
duhdanube <Danny.B.Lim@gmail.com>
2016-08-19 00:04:34 -0700

    Routes set up

--- routes/messages.js
+++ routes/messages.js
@@ -9,5 +9,27 @@ router.route('/')
       res.status(err ? 400: 200).send(err || messages);
     })
   })
+  .post((req, res) => {
+    Message.create(req.body, (err, newMessage) => {
+      res.status(err ? 400: 200).send(err || newMessage);
+    })
+  })
+
+router.get('/:id')
+  .get((req, res) => {
+    Message.findById(req.params.id, (err, message) => {
+      res.status(err ? 400: 200).send(err || message);
+    })
+  })
+  .delete((req, res) => {
+    Message.findByIdAndRemove(req.params.id, err => {
+      res.status(err ? 400: 200).send(err || `Message ${req.params.id} was deleted!`);
+    })
+  })
+  .put((req, res) => {
+    Message.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true}, (err, message) => {
+      res.status(err ? 400: 200).send(err || message);
+    })
+  })
 
 module.exports = router;
